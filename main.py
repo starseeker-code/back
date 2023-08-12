49c1941b10f3835a3f1d63a1a8b8cde88eef8c9c
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2023-08-12 19:32:25 +0200

    fixed route

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def get_date():
         raise HTTPException(status_code=404, detail="No date stored")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8081, reload=True)
